2a297bb55b1d70db56979e60eb826440e25a743c
Nyon <k.r.shoegaze@gmail.com>
2024-08-17 16:38:42 +0900

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "prettier"],
-  parserOptions: {
-    ecmaVersion: 12,
-  },
+  extends: ["eslint:recommended", "plugin:eslint-comments/recommended", "prettier"],
+  overrides: [],
   rules: {},
 };
